c2cb73487bc694c005e3e98051a26195bb08d4a5
ぴょん <eikostarsmile@yahoo.co.jp>
2016-09-27 16:43:56 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$consumerKey = "AkCe3qtcjeSvWellVnVPoTcsW";
-$consumerSecret = "3CtsnZg8pJh1xOVp9yxpSFy66tisA1JXuV8DbLuEApSfWEJYf2";
-$accessToken = "776698205778026496-RRLwrFbRVSiWuZnzZvZXBVuoMOoa0bX";
-$accessTokenSecret = "NuaosVtD9ung12mIttjl2jhQInrIeVv8naIAqLjhufy8t";
+$consumerKey = "ConsumerKeyの値をコピペ";
+$consumerSecret = "ConsumerSecretの値をコピペ";
+$accessToken = "accessTokenの値をコピペ";
+$accessTokenSecret = "acccessTokenSecretの値をコピペ";
 
 ?>
